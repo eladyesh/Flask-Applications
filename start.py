8d9944252ae24b7daa30dfe7c8a5b1d1a9331f3d
eladyesh <102996033+eladyesh@users.noreply.github.com>
2023-03-04 16:12:25 +0200

    Create start.py

---
+++ start.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template_string
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template_string('''
+        <!DOCTYPE html>
+        <html>
+        <head>
+            <title>{{ title }}</title>
+        </head>
+        <body>
+            <h1>Welcome to my Flask app!</h1>
+            <p>This is the home page.</p>
+        </body>
+        </html>
+    ''', title='Home')
+
+@app.route('/about')
+def about():
+    return render_template_string('''
+        <!DOCTYPE html>
+        <html>
+        <head>
+            <title>{{ title }}</title>
+        </head>
+        <body>
+            <h1>About Us</h1>
+            <p>We are a small company based in Flaskville.</p>
+        </body>
+        </html>
+    ''', title='About')
+
+if __name__ == '__main__':
+    app.run(debug=True)
